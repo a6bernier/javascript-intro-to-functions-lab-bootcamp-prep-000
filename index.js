a0d89fc617a7ae3533d26e85230836fff3301564
Andrew Bernier <a6bernier@gmail.com>
2018-06-26 23:16:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout("say hi to grandma") {
+function logShout(say hi to grandma) {
   console.log("I can't hear you!")
 }
