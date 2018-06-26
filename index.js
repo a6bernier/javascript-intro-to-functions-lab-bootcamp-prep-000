ea5942d716775ab5889689702c93a26fd32b85d5
Andrew Bernier <a6bernier@gmail.com>
2018-06-26 23:12:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,20 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function sayHiToGrandma(string) {
-  return string.toLowerCase("hello")
+function logShout(string) {
   console.log("I can't hear you!")
 }
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
