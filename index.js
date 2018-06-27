99751ca16484a709a4bbcf48baa453b6d0ca8be5
Andrew Bernier <a6bernier@gmail.com>
2018-06-27 00:29:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var uppercase = "I LOVE YOU, GRANDMA"
-var lowercase = "i love you, grandma"
-
 function shout(string) {
   return string.toUpperCase()
 }
+
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function sayHiToGrandma() {
   console.log("Hi, Grandma!")
   return `I can't hear you!`
