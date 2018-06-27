537d582aec66a4f186920e01aca76e97f47455c5
Andrew Bernier <a6bernier@gmail.com>
2018-06-27 03:33:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function logWhisper() {
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return `YES INDEED!`
-}
+  }
   if (string.toLowerCase() === string) {
     return `I can't hear you!`
-}
+  }
   if (string === "I love you, Grandma.") {
     return `I love you, too.`
   }
