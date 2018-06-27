9a785fc947a6405681023191b5bc497b2394c4f8
Andrew Bernier <a6bernier@gmail.com>
2018-06-27 00:31:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 function shout(string) {
   return string.toUpperCase()
 }
-
 function whisper(string) {
   return string.toLowerCase()
 }
-
 function sayHiToGrandma() {
   console.log("Hi, Grandma!")
   return `I can't hear you!`
