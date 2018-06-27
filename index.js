dce386e4e933663b622de51846875ac5ada4a768
Andrew Bernier <a6bernier@gmail.com>
2018-06-27 03:32:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return `I can't hear you!`
 }
+  if (string === "I love you, Grandma.") {
+    return `I love you, too.`
+  }
 }
